9fc19a23ac5de0695966e651354c5a37a327d896
FrankTII <frank.training2@gmail.com>
2022-12-06 22:04:05 -0600

    Solution to display get component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,20 @@
 import Banner from "./banner";
 import DataList from "./dataList";
 
+const listEndPoints=
+['','category','book','author','editorial','review','order',
+'users','users/signUp','users/logIn','library','rol']
+
+const indexEndPoint=2;
+const endPoint=listEndPoints[indexEndPoint];
+
 const App = () => {
    return (
       <>
       <Banner>
          <div>LibraryVerse!</div>
       </Banner>
-      <DataList/>
+      <DataList endPoint={endPoint}/>
       </>
    );
 };
--- src/components/dataList.js
+++ src/components/dataList.js
@@ -3,17 +3,18 @@ import DataRow from "./dataRow";
  //const URL="https://libroverse-production.up.railway.app";
 //const URL="http://localhost:4000"
 
-const DataList = () => {
+const DataList = ({endPoint}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
+  console.log("Endpint iniciando: DataList",endPoint);
+
  useEffect(() => {
   const getData = async () => {
     try {
-      const response = await fetch(
-        `/category`,{mode:'cors'}
+      const response = await fetch(endPoint,{mode:'cors'}
       );
+
       if (!response.ok) {
         throw new Error(
           `This is an HTTP error: The status is ${response.status}`
@@ -22,6 +23,7 @@ const DataList = () => {
       let actualData = await response.json();
       setData(actualData);
       setError(null);
+      console.log("data length from API:",Object.keys(actualData).length);
     } catch(err) {
       setError(err.message);
       setData(null);
@@ -30,7 +32,7 @@ const DataList = () => {
     }  
   }
   getData()
-}, [])
+}, [endPoint])
 
 
   
@@ -42,37 +44,81 @@ const DataList = () => {
 //     "updatedAt": "2022-11-12T20:57:03.000Z",
 //     "Books": [
 
-
-  return (
-    <div className="datalist">
-        <h5 className="themeFontColor text-center">
-          Data from LibraryVerse Category
-        </h5>
-      {loading && <div>A moment please...</div>}
-      {error && (
-        <div>{`There is a problem fetching the post data - ${error}`}</div>
-      )}
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Genre</th>
-            <th>description</th>
-            <th>CreatedAt</th>
+  
+console.log("data before return:Endpoint: ",endPoint, data)
+switch (endPoint){
+  case "category":
+    return (
+      <div className="datalist">
+          <h5 className="themeFontColor text-center">
+            Data from LibraryVerse Category
+          </h5>
+        {loading && <div>A moment please...</div>}
+        {error && (
+          <div>{`There is a problem fetching the post data - ${error}`}</div>
+        )}
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Genre</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+          {data &&
+            data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.genre}</td>
+            <td>{item.description}</td>
           </tr>
-        </thead>
-        <tbody>
-        {data &&
-          data.map(({ id, genre,description,createdAt,updatedAt}) => (
-            <DataRow key={id} genre={genre} description={description}
-              createdAt={createdAt}
-              updatedAt={updatedAt}
-            />
-              
           ))}      
-        </tbody>
-      </table>
-    </div>
-  );
+          </tbody>
+        </table>
+      </div>
+    );
+
+    case "book":
+      return(
+          <div className="datalist">
+              <h5 className="themeFontColor text-center">
+                Data from LibraryVerse Book
+              </h5>
+            {loading && <div>A moment please...</div>}
+            {error && (
+              <div>{`There is a problem fetching the post data - ${error}`}</div>
+            )}
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                {data&&Object.keys(data[0]).map((tablehead)=>(
+                  <th>{tablehead}</th>
+                ))}
+                  {/* <th>Genre</th>
+                  <th>Description</th> */}
+                </tr>
+              </thead>
+              <tbody>
+              {data &&
+                data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.book_name}</td>
+                <td>{item.description}</td>
+                <td>{item.publication_date}</td>
+                <td>{item.CategoryId}</td>
+                <td>{item.EditorialId}</td>
+                <td>{item.AuthorId}</td>
+                <td>{item.Author.name}</td>
+                <td>{item.Editorial.name}</td>
+                <td>{item.Category.genre}</td>
+              </tr>
+              ))}      
+              </tbody>
+            </table>
+          </div>
+      );
+  default:
+    return;
+  }
 }
 
 export default DataList;

063e21fe976d26473022b35f93484044d3a54dcc
Angel123Lara <adjls@gmail.com>
2022-12-08 03:32:09 -0600

    Se agrega pagina SearchResults y componentes de busqueda y menu

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 // import App from "./pages/App";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
+import SearchResult from "./pages/SearchResults";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Home />
+		<SearchResult/>
 	</React.StrictMode>
 );
---
+++ src/pages/SearchResults.js
@@ -0,0 +1,18 @@
+import React from "react";
+//import Banner from "../components/banner";
+import BarSearch from "../components/BarSearch";
+//import BasicMenu from "../components/MenuInSearchResults";
+
+function SearchResult(){
+    return(
+        <div>
+            <BarSearch ></BarSearch>    
+        
+            <h1>Hola Mundo d </h1>
+
+  
+        </div>
+    )
+}
+
+export default SearchResult;

4513036ac59feb9008d7f42f0f1916e3e4afa841
FrankTII <frank.training2@gmail.com>
2022-12-14 09:02:57 -0600

    Signup Added
    Call to token moved prom index.js/app.js to dashboard.js and preferences.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,15 +1,14 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import './Login.css';
 
 
 const base_URL="https://libroverse-production.up.railway.app/";
-//const URL="http://localhost:4000"
-const endPoint="users/logIn"
+//const URL="http://localhost:4000/"
+const endPoint="users/signup"
 const URL=base_URL+endPoint
 console.log("URL",URL)
 
-async function userLogIn(credentials) {
+async function userSignUp(credentials) {
     return fetch(URL,{
         method: 'POST',
         withCredentials: true,
@@ -27,23 +26,47 @@ async function userLogIn(credentials) {
       .then(data => data.json())
    }
 
-export default function Login({setToken}) {
+/*Example
+  "username":"admin22",
+   "firstname": "22",
+   "lastname": "22",
+   "address":"22",
+   "email": "admin22@localhost.com",
+   "userpass": "22222",
+   "phonenumber": "2",*/
+export default function SignUp({setToken}) {
     const [username, setUsername] = useState();
     const [userpass, setUserPass] = useState();  
+    const [firstname, setFirstname] = useState();
+    const [lastname, setLastname] = useState();
+    //const [address, setAddress] = useState();
+    const [email, setEmail] = useState();  
+    //const [phonenumber, setPhonenumber] = useState();  
 
-    const submitLogIn = async e => {
+    const submitSignUp = async e => {
         e.preventDefault();
-        const token = await userLogIn({
+        const token = await userSignUp({
           "username":username,
-          "userpass":userpass
+          "userpass":userpass,
+          "firstname": firstname,
+          "lastname": lastname,
+          "email": email
         });
         setToken(token);
       }
 
     return(
         <div className="login-wrapper">
-        <h1>Please Log In</h1>
-            <form onSubmit={submitLogIn}>
+        <h1>Please SignUp</h1>
+            <form onSubmit={submitSignUp}>
+            <label>
+                <p>Firstname</p>
+                <input type="text" onChange={e => setFirstname(e.target.value)}/>
+            </label>
+            <label>
+                <p>Lastname</p>
+                <input type="text" onChange={e => setLastname(e.target.value)}/>
+            </label>
             <label>
                 <p>Username</p>
                 <input type="text" onChange={e => setUsername(e.target.value)}/>
@@ -52,15 +75,18 @@ export default function Login({setToken}) {
                 <p>Password</p>
                 <input type="password" onChange={e => setUserPass(e.target.value)}/>
             </label>
+            <label>
+                <p>Email</p>
+                <input type="text" onChange={e => setEmail(e.target.value)}/>
+            </label>
             <div>
                 <button type="submit">Submit</button>
             </div>
             </form>
-            
         </div>
     )
 }
 
-Login.propTypes={
+SignUp.propTypes={
     setToken: PropTypes.func.isRequired
 }

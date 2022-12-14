6e6f1042f9cc344bafb23d102d1322564b7acdc9
Miguel Chavez <toonchavez8@gmail.com>
2022-12-14 13:21:09 -0600

    manually merge from token branch and add aalerts to refactord login

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,7 +5,7 @@ import "../css/Signup.css";
 export default function Signup() {
 	return (
 		<main className="DrawerOpen" style={{ marginLeft: "50rem" }}>
-			<div class="title">
+			<div className="title">
 				<h1 className="FS-128px FW-900 FF-Inter C-White">Dont Panic!</h1>
 				<p className="FS-30px FW-900 FF-Inter C-Secondary-Red-500">
 					All these can be yours, just SignUp!
